95bcfaaf689433cb606856fd5bf99e5de5098cfa
Zeke McCrary <zekemc2020@gmail.com>
2023-11-22 16:20:30 -0500

    Implemented FromStr trait for Polynomial

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod polynomial;
-use polynomial::{Polynomial, PolynomialStringError};
+use polynomial::Polynomial;//, PolynomialStringError};
 //use std::io;
 
 fn main() {
@@ -7,9 +7,9 @@ fn main() {
     println!("Here's the how it should look in case you forgot:\n(3x^3 + x^2 + 26x - 5)(x^2 + 4x)(x^16 - 12x^9 + -2)... etc.");
     println!("So go ahead:\n");
 
-    let mut _inp = String::new();
-
     let poly = Polynomial::new(  vec![ [9.0, 6.0], [1.0, 5.0], [2.0, 4.0], [0.0, 3923.1206309], [4.0, 2.0], [13.0, 1.0], [6.0, 0.0] ]  );
+    println!("{}\n", poly);
 
-    println!("{}\n\n", poly);
+    let poly_str = "3x^3 + 33.2 - 4x + -16.998x^33.3";
+    println!("{}", poly_str.parse::<Polynomial>().expect("Could not convert to polynomial"));
 }
--- src/polynomial.rs
+++ src/polynomial.rs
@@ -5,8 +5,7 @@ use std::str::FromStr;
 #[derive(Debug)]
 pub struct PolynomialStringError {
     pub message: String,
-    pub polynomial: String,
-    pub index: usize,
+    pub problem: char
 }
 
 impl std::error::Error for PolynomialStringError {}
@@ -14,10 +13,9 @@ impl std::error::Error for PolynomialStringError {}
 impl fmt::Display for PolynomialStringError {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         write!(formatter,
-            "{m}\n{p}\n{s}^",
+            "{m}\nIssue: {p}",
             m = &self.message,
-            p = &self.polynomial,
-            s = &( String::from_utf8(vec![32; self.index]).expect("Could not convert vec to string") ),
+            p = self.problem,
         )
     }
 }
@@ -66,4 +64,99 @@ impl fmt::Display for Polynomial {
 
         write!(formatter, "{}", build)
     }
+}
+
+impl FromStr for Polynomial {
+    type Err = PolynomialStringError;
+
+    // TODO: fix too much code repetition in this function
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parsed = s.replace(" ", "").replace("-", "+-");
+        let mut build: Vec<[f64; 2]> = Vec::new();
+        let mut termer = [0.0, 0.0];
+
+        for term in parsed.split('+') {
+            // side effect of using split and replace("-", "+-")
+            if term.len() == 0 { continue; }
+
+            // consider adding double negation support in the future
+            if term == "-" {
+                return Err(Self::Err {
+                    message: String::from("Repeated negative sign"),
+                    problem: '-',
+                });
+            }
+
+            if term.contains("x") {
+                if term.contains("^") {
+                    let mut success = [false, false];
+
+                    for (i, num) in term.split("x^").enumerate() {
+                        if i > 1 {
+                            return Err(Self::Err{
+                                message: String::from("Using multiple x's in the same term"),
+                                problem: 'x',
+                            });
+                        }
+
+                        if let Ok(n) = num.parse::<f64>() {
+                            termer[i] = n;
+                            success[i] = true;
+                        } else {
+                            for c in term.chars() {
+                                if !c.is_numeric() && c != '.'
+                                { return Err(Self::Err{
+                                    message: String::from("Non-numeric coefficient"),
+                                    problem: c,
+                                }); }
+                            }
+
+                            return Err(Self::Err{ message: String::from("Non-numeric coefficient"), problem: '?'});
+                        }
+                    }
+                }
+                else {
+                    // it probably won't let me do this
+                    if let Ok(n) = term.replacen("x", "", 1).parse::<f64>() {
+                        termer = [n, 1.0];
+                    } else {
+                        for c in term.chars() {
+                            if !c.is_numeric() && c != '.'
+                            { return Err(Self::Err{
+                                message: String::from("Non-numeric coefficient"),
+                                problem: c,
+                            }); }
+                        }
+
+                        // in case there wasnt a non-numeric character in there which wouldnt make sense but wtv
+                        return Err(Self::Err{ message: String::from("Non-numeric coefficient"), problem: '?'});
+                    }
+                }
+            }
+            else {
+                // term should be just a number if the program gets here
+                if let Ok(n) = term.parse::<f64>() {
+                    termer = [n, 0.0];
+                } else {
+                    for c in term.chars() {
+                        if !c.is_numeric() && c != '.'
+                        { return Err(Self::Err{
+                            message: String::from("Non-numeric coefficient"),
+                            problem: c,
+                        }); }
+                    }
+
+                    return Err(Self::Err{ message: String::from("Non-numeric coefficient"), problem: '?'});
+                }
+            }
+
+            // a coefficient of 0 is pointless
+            if termer[0] == 0.0 { continue; }
+
+            build.push( [termer[0], termer[1]] );
+        }
+
+        Ok(Polynomial{ terms: build })
+    }
+
 }
