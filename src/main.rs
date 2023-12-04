29a05c31fb7eb71c3756f208e1a2d23756f0304f
Zeke McCrary <zekemc2020@gmail.com>
2023-12-03 23:58:44 -0500

    Fixed implementation of FromStr for Polynomial

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 mod polynomial;
-use polynomial::Polynomial;//, PolynomialStringError};
-//use std::io;
+use polynomial::{Polynomial, PolynomialStringError};
+use std::io::stdin;
 
-fn main() {
-    println!("Polynomial multiplier: for students, teachers, and everyone in between. If you're avoiding work, you're in the right place.\n");
+fn main() -> Result<(), PolynomialStringError> {
+    println!("Polynomial multiplier: for students, teachers, and everyone else. If you're avoiding work, you're in the right place.\n");
     println!("Here's the how it should look in case you forgot:\n(3x^3 + x^2 + 26x - 5)(x^2 + 4x)(x^16 - 12x^9 + -2)... etc.");
     println!("So go ahead:\n");
 
-    let poly = Polynomial::new(  vec![ [9.0, 6.0], [1.0, 5.0], [2.0, 4.0], [0.0, 3923.1206309], [4.0, 2.0], [13.0, 1.0], [6.0, 0.0] ]  );
-    println!("{}\n", poly);
+    let mut buf = String::new();
+    stdin().read_line(&mut buf).expect("Failed to read line");
+    let poly_string = buf.trim_end_matches(|c| c == '\n' || c == '\r');
 
-    let poly_str = "3x^3 + 33.2 - 4x + -16.998x^33.3";
-    println!("{}", poly_str.parse::<Polynomial>().expect("Could not convert to polynomial"));
+    let poly = poly_string.parse::<Polynomial>()?;
+    println!("{:?}", poly);
+    println!{"{}", poly};
+
+    Ok(())
 }
--- src/polynomial.rs
+++ src/polynomial.rs
@@ -3,20 +3,22 @@ use std::string::ToString;
 use std::str::FromStr;
 
 #[derive(Debug)]
-pub struct PolynomialStringError {
-    pub message: String,
-    pub problem: char
+pub enum PolynomialStringError {
+    EmptyStringError,
+    ParseError(char),
+    ParenthesesError(bool),
 }
 
 impl std::error::Error for PolynomialStringError {}
 
 impl fmt::Display for PolynomialStringError {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(formatter,
-            "{m}\nIssue: {p}",
-            m = &self.message,
-            p = self.problem,
-        )
+        use PolynomialStringError::*;
+        match self {
+            EmptyStringError    => write!(formatter, "No polynomial found in string"                              ),
+            ParseError(c)    => write!(formatter, "Failure to parse, encountered character: {}", c      ),
+            ParenthesesError(b) => write!(formatter, "Illegal {} parentheses", if *b { "open" } else { "close" }  ),
+        }
     }
 }
 
@@ -38,22 +40,32 @@ impl fmt::Display for Polynomial {
         let mut build = String::with_capacity(num_terms * 5 + 4); // 5(len - 1) + 4 = 5(len) - 1
 
         for (i, [coeff, exp]) in self.terms.iter().enumerate() {
-            // somebody please explain to me why i have to manually dereference
-            match *coeff as i32 {
+            // somebody please explain to me why i have to explicitly dereference
+            // also please look past the ugly match statement used to trick the compiler into letting me use one
+            match *coeff {
                 // 0 * anything = 0
-                0 => continue,
+                n if n == 0.0 => continue,
 
                 // 1 * x = x
-                1 => match *exp as i32 {
-                    0 => build.push('1'), // 1x^0 = 1
-                    1 => build.push('x'), // 1x^1 = x
-                    _ => build.push_str( &format!("x^{val}", val = &exp.to_string()) ),
+                n if n == 1.0 =>
+                match *exp {
+                    n if n == 0.0 => build.push('1'), // 1x^0 = 1
+                    n if n == 1.0 => build.push('x'), // 1x^1 = x
+                    _             => build.push_str( &format!("x^{val}", val = &exp.to_string()) ),
                 },
 
-                _ => match *exp as i32 {
-                    0 => build.push_str( &coeff.to_string() ), // x^0 = 1
-                    1 => build.push_str( &format!("{te}x", te = &coeff.to_string()) ), // x^1 = x
-                    _ => build.push_str( &format!("{te}x^{ex}", te = &coeff.to_string(), ex = &exp.to_string()) ),
+                n if n == -1.0 =>
+                match *exp {
+                    n if n == 0.0 => build.push_str("-1"),
+                    n if n == 1.0 => build.push_str("-x"),
+                    _             => build.push_str( &format!("-x^{val}", val = &exp.to_string()) ),
+                }
+
+                _             =>
+                match *exp {
+                    n if n == 0.0 => build.push_str( &coeff.to_string() ), // x^0 = 1
+                    n if n == 1.0 => build.push_str( &format!("{te}x", te = &coeff.to_string()) ), // x^1 = x
+                    _             => build.push_str( &format!("{te}x^{ex}", te = &coeff.to_string(), ex = &exp.to_string()) ),
                 },
             }
             
@@ -69,94 +81,189 @@ impl fmt::Display for Polynomial {
 impl FromStr for Polynomial {
     type Err = PolynomialStringError;
 
-    // TODO: fix too much code repetition in this function
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parsed = s.replace(" ", "").replace("-", "+-");
-        let mut build: Vec<[f64; 2]> = Vec::new();
-        let mut termer = [0.0, 0.0];
-
-        for term in parsed.split('+') {
-            // side effect of using split and replace("-", "+-")
-            if term.len() == 0 { continue; }
-
-            // consider adding double negation support in the future
-            if term == "-" {
-                return Err(Self::Err {
-                    message: String::from("Repeated negative sign"),
-                    problem: '-',
-                });
-            }
-
-            if term.contains("x") {
-                if term.contains("^") {
-                    let mut success = [false, false];
-
-                    for (i, num) in term.split("x^").enumerate() {
-                        if i > 1 {
-                            return Err(Self::Err{
-                                message: String::from("Using multiple x's in the same term"),
-                                problem: 'x',
-                            });
-                        }
-
-                        if let Ok(n) = num.parse::<f64>() {
-                            termer[i] = n;
-                            success[i] = true;
-                        } else {
-                            for c in term.chars() {
-                                if !c.is_numeric() && c != '.'
-                                { return Err(Self::Err{
-                                    message: String::from("Non-numeric coefficient"),
-                                    problem: c,
-                                }); }
-                            }
-
-                            return Err(Self::Err{ message: String::from("Non-numeric coefficient"), problem: '?'});
-                        }
-                    }
-                }
-                else {
-                    // it probably won't let me do this
-                    if let Ok(n) = term.replacen("x", "", 1).parse::<f64>() {
-                        termer = [n, 1.0];
-                    } else {
-                        for c in term.chars() {
-                            if !c.is_numeric() && c != '.'
-                            { return Err(Self::Err{
-                                message: String::from("Non-numeric coefficient"),
-                                problem: c,
-                            }); }
-                        }
-
-                        // in case there wasnt a non-numeric character in there which wouldnt make sense but wtv
-                        return Err(Self::Err{ message: String::from("Non-numeric coefficient"), problem: '?'});
-                    }
+        enum Mode {
+            Number(char),
+            X,
+            Operator(bool),
+            Arrow,
+        }
+        
+        impl Mode {
+            // TODO: change to only use one return keyword
+            const fn new(c: char) -> Result<Self, PolynomialStringError> {
+                return match c {
+                    '^'                     => Ok(  Self::Arrow                                      ),
+                    'x' | 'X'               => Ok(  Self::X                                          ),
+                    '+' | '-'               => Ok(  Self::Operator(if c == '+' {true} else {false})  ),
+                    '.'                     => Ok(  Self::Number(c)                                  ),
+                    _ if c.is_ascii_digit() => Ok(  Self::Number(c)                                  ),
+        
+                    _                       => Err(PolynomialStringError::ParseError(c)),
                 }
             }
-            else {
-                // term should be just a number if the program gets here
-                if let Ok(n) = term.parse::<f64>() {
-                    termer = [n, 0.0];
-                } else {
-                    for c in term.chars() {
-                        if !c.is_numeric() && c != '.'
-                        { return Err(Self::Err{
-                            message: String::from("Non-numeric coefficient"),
-                            problem: c,
-                        }); }
-                    }
-
-                    return Err(Self::Err{ message: String::from("Non-numeric coefficient"), problem: '?'});
+        
+            // TODO: change to only use one return keyword
+            fn char(&self) -> char {
+                use Mode::*;
+                match self {
+                    Number(c)   => *c,
+                    Operator(b) => if *b {'+'} else {'-'},
+                    X           => 'x',
+                    Arrow       => '^',
                 }
             }
+        }
+        
 
-            // a coefficient of 0 is pointless
-            if termer[0] == 0.0 { continue; }
-
-            build.push( [termer[0], termer[1]] );
+        struct Expected<'a> { modes: &'a [Mode] }
+        
+        impl Expected<'_> {
+            fn matches(&self, mode: &Mode) -> Result<Self, PolynomialStringError> {
+                use std::mem::discriminant;
+        
+                for m in self.modes {
+                    if discriminant(m) != discriminant(mode) { continue; }
+        
+                    // TODO: change to only use one return keyword
+                    use Mode::*;
+                    return match mode {
+                        Number(_)   => Ok(Expected{ modes: &[ Number(0 as char), Operator(false), X, Arrow ] }),
+                        X           => Ok(Expected{ modes: &[ Operator(false), Arrow ] }),
+                        Operator(_) => Ok(Expected{ modes: &[ Number(0 as char), X ] }),
+                        Arrow       => Ok(Expected{ modes: &[ Number(0 as char), Operator(false), Arrow ] }),
+                    }
+                }
+        
+                Err(PolynomialStringError::ParseError(mode.char()))
+            }
         }
 
-        Ok(Polynomial{ terms: build })
+
+        fn parse_float(fstr: &mut String, err: char) -> Result<f64, PolynomialStringError> {
+
+            if let Ok(n) = fstr.parse::<f64>() {
+                *fstr = String::new();
+                return Ok(n);
+            }
+        
+            Err(PolynomialStringError::ParseError(err))
+        }
+
+
+        let input = format!("{}+", s);
+
+        let mut parsed: Vec<[f64; 2]> = Vec::new();
+        let mut expected: Expected = Expected { modes: &[Mode::Number(0 as char), Mode::X] };
+        let mut char_mode: Mode;
+        let mut arr_build = [f64::NAN; 2];
+        let mut num_build = String::new();
+        let mut sign_mode = true;
+
+        for c in input.chars() {
+            if c == ' ' { continue; }
+
+            char_mode = Mode::new(c)?;
+            expected = expected.matches(&char_mode)?;
+
+            match char_mode {
+                Mode::Number(c) => num_build.push(c),
+
+                /*
+                    Cases to handle:
+                    1. no coefficient was found (num_build is empty) ->
+                        set coefficient to 1
+                    2. coefficient was found ->
+                        parse num_build to coefficient
+                        set exponent to 1 (marker that an X was found in this term)
+                */
+                Mode::X => {
+                    // 1.
+                    if num_build.len() == 0 { num_build.push('1'); }
+
+                    // 2.
+                    if arr_build[0].is_nan() {
+                        arr_build[0] = parse_float(&mut num_build, 'x')?;
+                        arr_build[1] = 1.0;
+                    }
+                    else { return Err(PolynomialStringError::ParseError('x')); }
+                },
+
+                /*
+                    Cases to handle:
+                    1. no X was found ->
+                        parse num_build to coefficient
+                        apply coefficient sign
+                        set exponent to 0
+                        reset variables
+                    2. X was found but no arrow ->
+                        reset variables (everything else has already been done)
+                    3. X was found and arrow was found ->
+                        1. this operator is for the exponent ->
+                            set sign_mode
+                        2. this operator ends the term ->
+                            parse num_build to exponent
+                            apply exponent sign
+                            reset variables
+                */
+                Mode::Operator(b) => {
+                    let c = if b {'+'} else {'-'};
+
+                    // 1.
+                    if arr_build[0].is_nan() {
+                        arr_build[0] = parse_float(&mut num_build, c)? * (if sign_mode {1.0} else {-1.0});
+                        arr_build[1] = 0.0;
+                    }
+                    // 3.
+                    else if arr_build[1].is_nan() {
+                        // 3.1.
+                        if num_build.len() == 0 {
+                            sign_mode = b;
+                            continue;
+                        }
+                        // 3.2.
+                        else {
+                            arr_build[1] = parse_float(&mut num_build, c)?;
+                            if !sign_mode { arr_build[1] *= -1.0; }
+                        }
+                    }
+                    // 2. (in a way)
+                    else if arr_build[1] != 1.0 {
+                        return Err(PolynomialStringError::ParseError(c));
+                    }
+
+                    sign_mode = b;
+
+                    // reset for the next term
+                    parsed.push(arr_build);
+                    arr_build = [f64::NAN; 2];
+                },
+
+                /*
+                    Cases to handle:
+                    1. X has been found ->
+                        set exponent to NAN (marker that an arrow was found)
+                        apply coefficient sign
+                */
+                Mode::Arrow => {
+                    // 1.
+                    if !arr_build[0].is_nan() && arr_build[1] == 1.0 {
+                        arr_build[1] = f64::NAN;
+
+                        if !sign_mode {
+                            arr_build[0] *= -1.0;
+                            sign_mode = true;
+                        }
+                    }
+                    else {
+                        return Err(PolynomialStringError::ParseError('^'));
+                    }
+                },
+            }
+
+        }
+
+        Ok(Polynomial{ terms: parsed })
     }
 
 }
